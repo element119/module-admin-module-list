6df9469c49b4091c0695240e41db893eabcf5683
pykettk <pykettk@gmail.com>
2023-06-23 20:46:38 +0100

    Create Admin Grid of Modules

---
+++ Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Copyright © element119. All rights reserved.
+ * See LICENCE.txt for licence details.
+ */
+declare(strict_types=1);
+
+namespace Element119\AdminModuleList\Controller\Adminhtml\Index;
+
+use Magento\Backend\App\Action;
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\Page;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    public const ADMIN_RESOURCE = 'Element119_AdminModuleList::view';
+
+    public function __construct(
+        Context $context,
+        private readonly PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+    }
+
+    public function execute(): Page
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu(self::ADMIN_RESOURCE);
+        $resultPage->getConfig()->getTitle()->prepend(__('Modules'));
+
+        return $resultPage;
+    }
+}
